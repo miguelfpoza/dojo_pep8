f624946179c6b5a1f803d9c08e47796c89be9669
Miguel Angel Ferrero Poza <miguelfpoza@gmail.com>
2023-06-02 14:53:56 +0200

    Commit de la tarea

--- codebreaker.py
+++ codebreaker.py
@@ -1,34 +1,46 @@
-trueNumber = "1010";
+""" Clase Codebreaker.
+
+    Define método que implementa la lógica del juego.
+
+    Recibe un número, lo valida y verifica si el número recibido es igual al
+    número que se el jugador debe adinivnar, establecido a través de la
+    constante NUMERO_CORRECTO.
+
+    Si el número recibido es correcto pero no igual al NUMERO_CORRECTO,
+    se ejecuta la lógica que determinará cuantos números se han acertado y si
+    su posición coincide con la del NUMERO_CORRECTO.
+"""
+
+
+NUMERO_CORRECTO = "1010"
+
 
 class Codebreaker:
-    
-  def adivinar(self, numero=None):
-    if trueNumber == '':
-      return 'Number is not defined'
 
-    if numero is None or len(numero) != 4 or 'e' not in list(numero):
-      return "error"
-    
-    if numero == trueNumber:
-      return True
+    def adivinar(self, numero=None):
+        if NUMERO_CORRECTO == '':
+            return 'El número a adivinar no ha sido asignado'
 
-    resultadoX  = ''
-    resultado_  = ''
-    arrayNumber = []
-    
-    for x in len(numero):
-      if(arrayNumber[numero[x]] == True):
-        return 'error'
+        if numero is None or len(numero) != 4 or numero.isalpha():
+            return "Error"
 
-      arrayNumber[numero[x]] = True
-    
-    numero = list(numero)
+        if numero == NUMERO_CORRECTO:
+            return True
 
-    for index, x in numero:
-      if trueNumber[index] == numero[index]:
-        resultadoX+='X'
+        resultado_ = ''  # Almacena coincidencias de cifra pero no de posición
+        resultado_x = ''  # Almacena coincidencia de cifra y posición
 
-      elif x in trueNumber:
-        resultado_='_'
+        numero = list(numero)
 
-    return resultadoX+resultado_
+        """
+         Recorro el número recibido comprobando coincidencias de cifra
+         y posición
+        """
+
+        for index, cifra in enumerate(numero):
+            if NUMERO_CORRECTO[index] == cifra:
+                resultado_x += 'X'
+            elif cifra in NUMERO_CORRECTO:
+                resultado_ = '_'
+
+        return resultado_x + resultado_
--- main.py
+++ main.py
@@ -1,18 +1,39 @@
+""" Entrada al juego CodeBreaker.
+
+    Establece mediante la constante INTENTOS_TOTALES el número de intentos
+    máximos que tiene el jugador para adivinar un número.
+
+    A continuación pide al jugador que introduzca un número.
+
+    Se llama al método 'adivinar' de la clase 'Codebreaker', encargado de 
+    verificar el número introducido por el jugador.
+
+    La lógica se repite hasta que el jugador introduzca el número correcto o 
+    se supere el número de intentos.
+"""
+
+
 from codebreaker import Codebreaker
 
-intentos_totales = 10
+INTENTOS_TOTALES = 10
+
 codebreaker = Codebreaker()
 
 intento = 0
 
-print('Jugar Codebreaker!')
+print('Vamos a Jugar Codebreaker!')
 
-while intento != intentos_totales:
-   number = input('Numero:');
-   resolve = codebreaker.adivinar(number)
-   print(resolve)
-   if resolve == True:
-      print('You win!!')
-      break
+while intento != INTENTOS_TOTALES:
+    numero = input('Numero:')
 
+    solucion = codebreaker.adivinar(numero)
 
+    if solucion is True:
+        print('Has ganado!!')
+        break
+    else:
+        print(solucion)
+        intento += 1
+
+if intento == INTENTOS_TOTALES:
+    print('Ooohhh, has perdido. Inténtalo de nuevo!!')
